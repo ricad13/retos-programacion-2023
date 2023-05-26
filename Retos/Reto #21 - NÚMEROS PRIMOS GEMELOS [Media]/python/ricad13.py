11a5ced3ae51e5262e41d7fa03c0ba2bf39ac751
ricad13 <134743085+ricad13@users.noreply.github.com>
2023-05-26 23:36:14 +0200

    Reto#21-python

---
+++ Retos/Reto #21 - NÚMEROS PRIMOS GEMELOS [Media]/python/ricad13.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+def primos_gemelos(rango):
+    resul=[]
+    pg=False
+    for i in range(3,rango,2):
+        p=True
+        for x in range(2,int(i**0.5)+1):
+            if i%x==0:
+                p=False
+                break
+        if p:
+            if pg:
+                resul.append((i-2,i))
+            pg=True
+        else:
+            pg=False
+    return resul
+            
+print(primos_gemelos(1000))
+
+
+
+
+
+
